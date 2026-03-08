7f45c702206e516cd2bb0d095fbfa11362f21058
Andrew <and.boj.70@gmail.com>
2026-03-08 16:05:04 +0200

    Lab 1

---
+++ lab1/test.js
@@ -0,0 +1,22 @@
+function* Generation(){
+    let a = 1n, b = 1n, c = 1n;
+    while(true){
+        yield c;
+        c = a + b;
+        b = a;
+        a = c;
+    }
+}
+
+
+const Iteration = (generator, timer) => {
+    const end = new Date().getTime() + timer*1000;
+    const iterable = generator;
+    const iterator = iterable[Symbol.iterator]();
+    while (new Date().getTime() < end){
+        const res = iterator.next();
+        console.log(`Value: ${res.value} \n`);
+  }
+};
+
+Iteration(Generation(), 0.1);
