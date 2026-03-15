558a57a53dd40bc6c2d7e50bbafae562ae5559f0
Andrew <and.boj.70@gmail.com>
2026-03-15 14:16:20 +0200

    Change modules and exports

--- lab2/main.js
+++ lab2/main.js
@@ -1,7 +1,22 @@
-import {FibonachiGeneration} from "./fibonachi_generator.js";  
-import {AlphabetGeneration} from "./alphabet_generator.js";
-import {Iteration} from "./iterator.js";
+import {FibonachiGeneration} from "labs2/fibonachi";  
+import {AlphabetGeneration} from "labs2/alphabet";
+import {Color} from "labs2/colors";
+
+
+export const Iteration = (generator, timer) => {
+    const end = new Date().getTime() + timer*1000;
+    const iterable = generator;
+    const iterator = iterable[Symbol.iterator]();
+    const colorGen = Color();
+
+    while (new Date().getTime() < end){
+        const res = iterator.next();
+        const color = colorGen.next().value;
+        console.log(color(`Value: ${res.value}`));
+    }
+};
 
 
 Iteration(FibonachiGeneration(), 0.01);
+console.log(" ");
 Iteration(AlphabetGeneration(), 0.01);
